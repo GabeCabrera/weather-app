8d59ba6a2b050e68cf99ebf44a45088c6849387f
GabeCabrera <gabrielmscabrera@gmail.com>
2019-03-02 20:29:25 -0700

    Updates

---
+++ precache-manifest.a82e76f77c5832beb3ab4ae7aaa04c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "d819ed13ad5a0da6a59e",
+    "url": "/weather-app/static/js/main.d819ed13.chunk.js"
+  },
+  {
+    "revision": "c71d62fb2371311dd058",
+    "url": "/weather-app/static/js/2.c71d62fb.chunk.js"
+  },
+  {
+    "revision": "d819ed13ad5a0da6a59e",
+    "url": "/weather-app/static/css/main.5eac10e8.chunk.css"
+  },
+  {
+    "revision": "7ff0da57dba45f5cb269f863bad7b325",
+    "url": "/weather-app/index.html"
+  }
+];
